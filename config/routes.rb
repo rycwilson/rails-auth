0509428c700f57f6f66dd0fd63325132338b3771
rycwilson <rycwilson@gmail.com>
2015-09-27 08:14:29 -0700

    changed logout request method to delete

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new', as: 'signup'
 
   get '/login', to: 'sessions#new', as: 'login'
-  get '/logout', to: 'sessions#destroy', as: 'logout'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
   post '/sessions', to: 'sessions#create'
 
   resources :users
